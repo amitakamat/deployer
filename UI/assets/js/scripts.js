d1e6f750aede5f6e11f1900c7f27563d6fcd8554
Aaditya Deowanshi <aadityadeowanshi@hotmail.com>
2017-05-06 12:32:08 -0700

    changes got post

--- UI/assets/js/scripts.js
+++ UI/assets/js/scripts.js
@@ -9,23 +9,32 @@ jQuery(document).ready(function() {
     /*
         Form validation
     */
-    $('.login-form input[type="text"], .login-form input[type="password"], .login-form textarea').on('focus', function() {
+/*    $('.login-form input[type="text"], .login-form input[type="password"], .login-form textarea').on('focus', function() {
     	$(this).removeClass('input-error');
-    });
+    });*/
     
-    $('.login-form').on('submit', function(e) {
-    	
-    	$(this).find('input[type="text"], input[type="password"], textarea').each(function(){
-    		if( $(this).val() == "" ) {
-    			e.preventDefault();
-    			$(this).addClass('input-error');
-    		}
-    		else {
-    			$(this).removeClass('input-error');
-    		}
-    	});
-    	
+    $('#login-form').submit(function(e) {
+        e.preventDefault();
+        var git_repo = document.getElementById("login-form")[1].value;
+        console.log(git_repo);
+        jQuery.ajax({
+         type: "POST",
+         url: "http://104.196.235.71/deployer/v1/6e2edab7-69be-43d0-a304-e655a708b811/deploy",
+         data: "{ git_url: git_repo}",
+         dataType:"json",
+         success: function(data, textStatus, jqXHR) {
+            console.log(data + textStatus);
+            setInterval(function(){
+            alert("Hello")},3000         
+            }, //function(data, textStatus, jqXHR)
+         error: function(jqXhr, textStatus, errorThrown) {
+         console.log(errorThrown); //function(jqXHR, textStatus, errorThrown)
+         }
+        });
     });
+
+
+
 $(function () {
 
 $('a[href^="#"]').click(function(event) {
